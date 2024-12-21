6f29fd00c2aeb38b887686ec154906100538efba
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2024-12-21 08:37:52 +0000

    add astro-icon

--- surrealtech/astro.config.mjs
+++ surrealtech/astro.config.mjs
@@ -3,9 +3,11 @@ import vercel from "@astrojs/vercel/serverless";
 
 import clerk from "@clerk/astro";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk(), icon()],
   
   output: "server",
   adapter: vercel(),
