60c10e598e3689b0dd9d54815c8c96dc619d00da
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2024-12-21 15:37:15 +0000

    add openprops.css

--- surrealtech/postcss.config.cjs
+++ surrealtech/postcss.config.cjs
@@ -3,6 +3,7 @@ const OpenProps = require('open-props');
 
 module.exports = {
   plugins: [
+    require('postcss-import'), // Enable importing CSS files
     postcssJitProps(OpenProps),
     require('autoprefixer'),
   ],
